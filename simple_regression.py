c1a3f97923e51848670f0e080143c06b0f01bc79
Shaon Majumder <shaon.fdr@robist.com>
2018-06-21 17:02:49 +0600

    simple regression

--- simple_regression.py
+++ simple_regression.py
@@ -6,7 +6,8 @@ x = np.array([1,2,3,4,5,6],dtype=np.float64)
 y = np.array([5,4,6,5,6,7],dtype=np.float64)
 
 def m_b(x,y):
-    m = ((mean(x)*mean(y)) - mean(x*y)) / ((mean(x)**2) - x**2)
+    m = ((mean(x)*mean(y)) - mean(x*y)) / ((mean(x)**2) - mean(x**2))
     b = mean(y)-m*mean(x)
     return m,b
 
+print(m_b(x,y))
