e669f28c2b6843679f1f9fd0368818ae8c30d923
Shaon Majumder <shaon.fdr@robist.com>
2018-06-21 16:54:48 +0600

    eq edited

---
+++ simple_regression.py
@@ -0,0 +1,12 @@
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([1,2,3,4,5,6],dtype=np.float64)
+y = np.array([5,4,6,5,6,7],dtype=np.float64)
+
+def m_b(x,y):
+    m = ((mean(x)*mean(y)) - mean(x*y)) / ((mean(x)**2) - x**2)
+    b = mean(y)-m*mean(x)
+    return m,b
+
