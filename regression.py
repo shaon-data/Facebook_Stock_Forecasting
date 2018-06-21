0a74d8e09f5006b55bf314efcd4b3af4eb993d67
Shaon Majumder <shaon.fdr@robist.com>
2018-06-21 12:11:10 +0600

    regression plottig comming

--- regression.py
+++ regression.py
@@ -1,12 +1,14 @@
+import quandl, math, datetime
 import pandas as pd
-import quandl
-import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib import style
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 
+style.use('ggplot')
+
 df = quandl.get('WIKI/GOOGL')
-print("Avialable labels = ",list(df))
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close'])/df['Adj. Close']*100.0
@@ -14,7 +16,6 @@ df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open']*100.0
 
 df = df[['Adj. Close','HL_PCT','PCT_Change','Adj. Volume']]
 
-print(df.head())
 forecast_col = 'Adj. Close'
 
 #instead of Nan, considering as outlier for not loosing nan row other information
@@ -25,11 +26,14 @@ forecast_out = int(math.ceil(0.01*len(df)))
 
 #Shift index by desired number of periods with an optional time freq
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop(['label'],1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
@@ -38,4 +42,5 @@ clf = LinearRegression()
 clf.fit(X_train, y_train) #train
 accuracy = clf.score(X_test, y_test) #test
 
-print(accuracy)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
