154c4521f7c4dad24a151913344a65d1ba21c5a2
Shaon Majumder <shaon.fdr@robist.com>
2018-06-23 15:29:04 +0600

    coef_deter

--- simple_regression.py
+++ simple_regression.py
@@ -6,12 +6,20 @@ from matplotlib import style
 style.use('fivethirtyeight')
 
 x = np.array([1,2,3,4,5,6],dtype=np.float64)
-y = np.array([2,3,4,5,4,6],dtype=np.float64)
+y = np.array([5,4,6,5,6,7],dtype=np.float64)
 
 def m_b(x,y):
     m = ((mean(x)*mean(y)) - mean(x*y)) / ((mean(x)**2) - mean(x**2))
     b = mean(y)-m*mean(x)
     return m,b
+
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+def coefficient_of_determination(ys_orig,ys_line):
+    squared_error_y_reg = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, mean(ys_orig) )
+    return 1 - (squared_error_y_reg/squared_error_y_mean)
 
 m,b= m_b(x,y)
 y_ = m*x + b
@@ -20,6 +28,8 @@ y_ = m*x + b
 prediction_x = 6.5
 prediction_y = m*prediction_x + b
 
+r_squared = coefficient_of_determination(y,y_)
+print(r_squared)
 plt.scatter(x,y, label='real data points', color='r')
 plt.scatter(prediction_x,prediction_y, label='single prediction', color='g')
 plt.plot(x,y_, label='prediction line')
